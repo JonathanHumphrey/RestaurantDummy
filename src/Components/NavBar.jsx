060f8cdab810fe348121cbdb75bb14198f1ff962
jonathanhumphrey <jlh473@txstate.edu>
2021-07-05 11:10:14 -0500

    initial commit

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import scss from './navbar.module.scss';
+
+export default function NavBar() {
+    return (
+        <div className={scss['NavBar']}>
+            <p>peepee</p>
+        </div>
+    )
+}
